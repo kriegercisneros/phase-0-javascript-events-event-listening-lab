1b23e40ef14a350d1a7aa572784006dec375afbe
JackieCisneros <krieger.jacqueline@gmail.com>
2023-02-01 15:35:01 -0700

    creating event listener function, locating my target, locating my event type, creating event listener function, creating the event handler function

--- index.js
+++ index.js
@@ -1,2 +1,32 @@
-function addingEventListener() {
+// function addingEventListener() {
+//     const input = document.getElementById('button');
+
+//     function clickAlert() {
+//     alert('I was clicked!');
+//     }
+
+//     input.addEventListener('click', clickAlert);
+// }
+// addingEventListener()
+
+
+// const input = document.getElementById('button');
+
+// function clickAlert() {
+//     alert('I was clicked!');
+// }
+
+// input.addEventListener('click', clickAlert);
+
+
+function addingEventListener(){
+
+    const input = document.getElementById('button');
+    function clickAlert(){
+        alert('I was clicked!');
+    };
+
+    input.addEventListener('click', clickAlert());
 }
+
+addingEventListener();
